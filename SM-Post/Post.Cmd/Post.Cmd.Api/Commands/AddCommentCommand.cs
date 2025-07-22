4e6bc4ac9a900e71f21ac5a488cda69dd9ad8c0a
Mayank Bhadani <bhadanimayank1@gmail.com>
2025-07-22 20:27:01 +0530

    updated command classes

---
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Commands/AddCommentCommand.cs
@@ -0,0 +1,8 @@
+namespace Post.Cmd.Api.Commands
+{
+    public class AddCommentCommand
+    {
+        public string Comment {get; set;}
+        public string Username {get; set;}   
+    }
+}
--- SM-Post/Post.Query/Post.Query.Domain/obj/Debug/net9.0/Post.Query.Domain.AssemblyInfo.cs
+++ SM-Post/Post.Query/Post.Query.Domain/obj/Debug/net9.0/Post.Query.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Post.Query.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1738312e8da1dbcd8ccf18296483e4622e4e354e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07112344f2cb93e06e2cc990ddf591db9766a697")]
 [assembly: System.Reflection.AssemblyProductAttribute("Post.Query.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Post.Query.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
