3ebdda436338a4e7005a568e30f80cb57e0a40ac
Mayank Bhadani <bhadanimayank1@gmail.com>
2025-07-22 20:56:59 +0530

    Updated Event Classes

--- CQRS-ES/CQRS.Core/obj/Debug/net9.0/CQRS.Core.AssemblyInfo.cs
+++ CQRS-ES/CQRS.Core/obj/Debug/net9.0/CQRS.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CQRS.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07112344f2cb93e06e2cc990ddf591db9766a697")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e6bc4ac9a900e71f21ac5a488cda69dd9ad8c0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CQRS.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CQRS.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
